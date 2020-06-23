4766d07b7d9c2f9dc605eec72d0c0a944461a056
jing-q-xu <jing.q.xu@hotmail.com>
2020-06-23 14:51:43 +0800

    refactor

--- cmake/GTest.cmake
+++ cmake/GTest.cmake
@@ -1,18 +1,20 @@
 
 macro(build_gtest)
-    message("GOOGLETEST_ROOT:" ${GOOGLETEST_ROOT})
-    include_directories(
-        ${GOOGLETEST_ROOT}
-        ${GOOGLETEST_ROOT}/include
-        )
-    set(GOOGLETEST_SOURCES
-        ${GOOGLETEST_ROOT}/src/gtest-all.cc
-        ${GOOGLETEST_ROOT}/src/gtest_main.cc
-        )
-    foreach(_source ${GOOGLETEST_SOURCES})
-        set_source_files_properties(${_source} PROPERTIES GENERATED 1)
-    endforeach()
+    if (TARGET gtest)
+    else()
+        message("GOOGLETEST_ROOT:" ${GOOGLETEST_ROOT})
+        include_directories(
+            ${GOOGLETEST_ROOT}
+            ${GOOGLETEST_ROOT}/include
+            )
+        set(GOOGLETEST_SOURCES
+            ${GOOGLETEST_ROOT}/src/gtest-all.cc
+            ${GOOGLETEST_ROOT}/src/gtest_main.cc
+            )
+        foreach(_source ${GOOGLETEST_SOURCES})
+            set_source_files_properties(${_source} PROPERTIES GENERATED 1)
+        endforeach()
 
-    add_library(gtest ${GOOGLETEST_SOURCES})
-    install (TARGETS gtest DESTINATION lib)
+        add_library(gtest ${GOOGLETEST_SOURCES})
+    endif()
 endmacro()
